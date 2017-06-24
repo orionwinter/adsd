6ca0a5116b99e2fe65fe610f33a8bef24bb629b6
Órion Winter <orion.lima@ccc.ufcg.edu.br>
2017-06-24 15:39:29 -0300

    Fazendo requisição

--- projeto-medicao/index.js
+++ projeto-medicao/index.js
@@ -1,18 +1,34 @@
-var fs = require('fs')
+var fs = require("fs"),
+    rp = require("request-promise");
 
-for (var i = 0; i < 10; i++) {
-    var antes = new Date();
+var appendOnFile = function(before, gotError, i) {
+    var after = new Date();
 
-    // TODO Fazer requisição para o best-trip-recommender
+    var text = i + "," + Number(before) + "," + Number(after) + "," + Number(after - before) + "," + gotError + "\n";
 
-    var depois = new Date();
-
-    var text = Number(antes) + "," + Number(depois) + "," + Number(depois - antes) + "\n";
-
-    fs.appendFile("/home/orion/Documentos/adsd/projeto-medicao/output.txt", text, function(err) {
+    fs.appendFile(__dirname + "/output.csv", text, function(err) {
         if(err) {
-          return console.log(err);
-}
-        console.log("The file was saved!");
+            return console.log(err);
+        }
+        if (!gotError){
+            console.log("The file was saved!");
+        }
     });
+};
+
+var makeRequest = function(i) {
+    var before = new Date();
+    rp("http://150.165.85.4:10400/get_best_trips?route=507&time=17:00:00&date=2017-02-04&bus_stop_id=26255&closest_trip_type=single_trip")
+        .then(function (response) {
+            appendOnFile(before, false, i);
+        })
+        .catch(function (err) {
+            appendOnFile(before, true, i);
+        });
+};
+
+// TODO: Fazer a quantidade de requisições por minuto necessárias
+for (var i = 0; i < 10; i++) {
+    makeRequest(i);
 }
+
