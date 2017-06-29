2a5d0b9ba19f6f1244a68aab051028e597eef656
odwl66 <orion.lima@ccc.ufcg.edu.br>
2017-06-29 09:35:33 -0300

    Adicionando os dados

--- projeto-medicao/index.js
+++ projeto-medicao/index.js
@@ -3,14 +3,15 @@ var fs = require("fs"),
 
 var start = new Date();
 
-var argument = process.argv.slice(2, 3);
+var argument = process.argv.slice(2, 3),
+    directory = process.argv.slice(3, 4);
 
-var appendOnFile = function(before, gotError, i) {
+var appendOnFile = function(before, gotError, i, directory) {
     var after = new Date();
 
     var text = Number(start) + "," + argument + "," + i + "," + Number(before) + "," + Number(after) + "," + Number(after - before) + "," + gotError + "\n";
 
-    fs.appendFile(__dirname + "/output.csv", text, function(err) {
+    fs.appendFile(directory + "/output.csv", text, function(err) {
         if(err) {
             return console.log(err);
         }
@@ -20,22 +21,24 @@ var appendOnFile = function(before, gotError, i) {
     });
 };
 
-var makeRequest = function(i) {
+var makeRequest = function(i, directory) {
     var before = new Date();
     rp("http://150.165.85.4:10400/get_best_trips?route=507&time=17:00:00&date=2017-02-04&bus_stop_id=26255&closest_trip_type=single_trip")
         .then(function (response) {
-            appendOnFile(before, false, i);
+            appendOnFile(before, false, i, directory);
         })
         .catch(function (err) {
-            appendOnFile(before, true, i);
+            appendOnFile(before, true, i, directory);
         });
 };
 
 if (argument == "") {argument = 5;}
+if (directory == "") {directory = __dirname;}
+
 
 console.log("Irei executar " + argument + " requisições");
 
 for (var i = 0; i < Number(argument); i++) {
-    makeRequest(i);
+    makeRequest(i, directory);
 }
 
